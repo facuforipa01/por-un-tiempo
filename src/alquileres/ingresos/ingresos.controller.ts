b218e9081f3a7e1f75ab1c3498894411d33dcbc4
facuforipa <facufolatelli2001@gmail.com>
2024-09-19 22:06:00 -0300

    ingresos y parcelas Get methods

--- src/alquileres/ingresos/ingresos.controller.ts
+++ src/alquileres/ingresos/ingresos.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Query, Res } from '@nestjs/common';
 import { IngresosService } from './ingresos.service';
 import { IngresoDto } from './ingresos.dto';
+import { PaginationQueryDto } from 'src/common';
+import { Response } from 'express';
 
 @Controller('ingresos')
 export class IngresosController {
 
     constructor(private readonly service: IngresosService) {}
 
+    //CARGAR UNA ENTRADA
     @Post('entrada')
     async ocuparParcela(
         @Body() 
@@ -17,5 +20,19 @@ export class IngresosController {
         const result = this.service.ocuparParcela(usuario, parcela);
     
         return result;
+    }
+
+    //OBTENER UN INGRESO
+    @Get(':id')
+    async getOne(@Param('id') id: number, @Res() response: Response){
+        const ingreso = await this.service.getOne(id);
+     response.status(HttpStatus.OK).json({ ok: true, ingreso, msg: 'approved' })
+    }
+
+    //OBTENER LISTADO DE INGRESOS
+    @Get('/')
+    async getAll(@Query() paginationQuery: PaginationQueryDto, @Res() response: Response){
+        const ingresos = await this.service.getAll(paginationQuery);
+     response.status(HttpStatus.OK).json({ ok: true, ingresos, msg: 'approved' })
     }
 }
--- src/alquileres/ingresos/ingresos.service.ts
+++ src/alquileres/ingresos/ingresos.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { Get, HttpException, HttpStatus, Injectable, NotFoundException, Param, Query, Res } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Ingreso } from './ingresos.entity';
 import { QueryFailedError, Repository } from 'typeorm';
@@ -7,16 +7,18 @@ import { ParcelaDto } from '../parcelas/parcelas.dto';
 import { Parcela } from '../parcelas/parcelas.entity';
 import { Usuarios } from 'src/usuarios/usuarios.entity';
 import { UsuarioDto } from 'src/usuarios/usuarios.dto';
+import { Response } from 'express';
+import { PaginationQueryDto } from 'src/common';
 
 @Injectable()
 export class IngresosService {
     constructor(
         @InjectRepository(Ingreso)
-        private readonly repo: Repository<IngresoDto>,
+        private readonly ingresoRepository: Repository<IngresoDto>,
         @InjectRepository(Parcela)
-        private readonly parcela: Repository<ParcelaDto>,
+        private readonly parcelaRepositoy: Repository<ParcelaDto>,
         @InjectRepository(Usuarios)
-        private readonly usuario: Repository<UsuarioDto>
+        private readonly usuarioRepository: Repository<UsuarioDto>
     ) { }
 
   /**   async guardarIngreso(
@@ -54,7 +56,7 @@ export class IngresosService {
     // cambiar a true la ocupacion parcela/ocupacion
 
     async ocuparParcela(parcelaid: number, usuarioid: number): Promise<ParcelaDto> {
-        const parcela = await this.parcela.findOneBy({ id: parcelaid });
+        const parcela = await this.parcelaRepositoy.findOneBy({ id: parcelaid });
 
         if (!parcela) throw new NotFoundException('Parcela no encontrada');
 
@@ -69,9 +71,9 @@ export class IngresosService {
         nuevoIngreso.entrada = new Date(Date.now() * 1000)
         nuevoIngreso.parcela = parcela
 
-        await this.repo.save(nuevoIngreso);
+        await this.ingresoRepository.save(nuevoIngreso);
  
-        return this.parcela.save(parcela)
+        return this.parcelaRepositoy.save(parcela)
       
 }
 
@@ -81,6 +83,44 @@ export class IngresosService {
     // cargar la fecha actual en ingresos/salida
     // cambiar a false la ocupacion parcela/ocupacion
 
+
+    async getOne(id: number): Promise<IngresoDto> {
+        try {
+            const ingreso = await this.ingresoRepository.findOne({where: {id}});
+        if (!ingreso) throw new NotFoundException('no encontramos ninguna parcela con ese id')
+            return ingreso;
+        } catch (err) {
+            console.error(err)
+      if (err instanceof QueryFailedError)
+        throw new HttpException(`${err.name} ${err.driverError}`, 404);
+      throw new HttpException(err.message, err.status)
+        }
+        
+    }
+    async getAll(paginationQuery: PaginationQueryDto): Promise<{
+        data: IngresoDto[];
+        total: number;
+        page: number;
+        limit: number;
+      }> {
+        const {page = 1, limit = 10} = paginationQuery;
+        try {
+            const [ingresos, total] = await this.ingresoRepository.findAndCount({
+                skip: (page - 1) * limit,
+                take: limit
+              })
+            const ingreso = await this.ingresoRepository.find();
+        if (!ingreso) throw new NotFoundException('no encontramos ninguna parcela con ese id')
+            return {data: ingresos, total, page, limit};
+        } catch (err) {
+            console.error(err)
+      if (err instanceof QueryFailedError)
+        throw new HttpException(`${err.name} ${err.driverError}`, 404);
+      throw new HttpException(err.message, err.status)
+        }
+        
+    }
+    
 }
 
 
